eae6fe4082f341340e884907a61810eae770f2cf
Ravi8889 <ravikiran.konda9999@gmail.com>
2021-11-25 23:56:04 +0530

    done

--- Interview Prepration Python/UndeRStanding Stats/untitled0.py
+++ Interview Prepration Python/UndeRStanding Stats/untitled0.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 23 17:23:26 2021
+Created on Thu Nov 25 10:54:53 2021
 
 @author: RaviKiran
 """
 
-tat
+# fizz and Buzz program
+for i in range(1,51):
+    if i % 3 == 0 and i % 5 ==0:
+        print( i ,"Fizz and Buzz ")
+        continue;
+    if i % 3 ==0:
+        print(i, "fizz")
+    if i % 5 ==0:
+        print(i,"Buzz")
+for i in range(1,51):
+    print(i)
