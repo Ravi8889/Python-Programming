261d0a22c7a32c075c10d2c16418c924b94d83e1
Ravi8889 <ravikiran.konda9999@gmail.com>
2021-12-05 23:54:33 +0530

    regular expressions

---
+++ Interview Prepration Python/Regular Expressions/Regular Expressions.py
@@ -0,0 +1,10 @@
+### \d ==> this is slash small  " d " which  is usefull to get the any information  related to " numbers "from the text or string.
+### \D ==> this is slash capital " D " which is usefull to get the any information  related to " Non Numbers "from the text or string.
+'''
+to get any single number from the string or document use \d  to get multiple string's use the  \d\d to get thrible numbers use  \d\d\d
+to get excatly how many number of occurences of the thing use ==> \d{number} or to get 10 number use ==> \d{10}
+ " | " ==> this is used for  or condition between two statements.
+ "
+ '''
+text = "Hello World this is RaviKiran KOnda  this is ravi kiran yes here i am givin the my number if you have any questions regarding the dogecoin Tesla reveenus is 9652580852 yes this is the number please call us at any time we will be help full to you thank you. yours lovingly RaviKiraKOndathe tesla efo number is (999)-333-999-7777 "
+#print(text)
